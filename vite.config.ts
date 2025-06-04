991cbc7d138e7538a960d6e142c05c45fa485399
Adham Akmal Azmi <adham_92@live.com>
2025-06-04 16:48:49 +0800

    feat: Update themes and implement new template for login page
    - Changed theme names from "keycloakify-starter" to "my-theme-1" and "my-theme-2" in kc.gen.tsx.
    - Updated KcPage.tsx to import the new Template component and added CSS import.
    - Created a new Template.tsx file for custom login page layout and functionality.
    - Added index.css to import Tailwind CSS styles.
    - Modified main.tsx to set the pageId for the mock context to "login.ftl".
    - Updated vite.config.ts to include Tailwind CSS plugin and specify theme names for Keycloakify.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { keycloakify } from "keycloakify/vite-plugin";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
+        tailwindcss(),
         keycloakify({
-            accountThemeImplementation: "none"
+            accountThemeImplementation: "none",
+            themeName: ["my-theme-1", "my-theme-2"],
+            keycloakVersionTargets: {
+                "22-to-25": true,
+                "all-other-versions": `${new Date().toISOString().replace('T', '_').replace(/:/g, '-')}.jar`,
+            }
         })
     ]
 });
